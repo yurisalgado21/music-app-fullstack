751a98651a44d62c4c969feb06414b473c423140
Yuri Salgado Farias Torres <yuri.salgado.ft@gmail.com>
2023-12-31 17:34:11 -0300

    adicionando o botão esqueci minha senha, para alterar no backend

--- frontend/music-app/src/components/Login.tsx
+++ frontend/music-app/src/components/Login.tsx
@@ -7,11 +7,26 @@ export default function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const navigate = useNavigate();
+  const [loginIsNotVisible, setLoginIsNotVisible] = useState(false);
 
   const formIsValid = () => {
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const isValid = userName.length >= 8 && regexEmail.test(email) && password.length >= 6;
     return isValid;
+  }
+  
+  const formNewPasswordIsValid = () => {
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const isValid = regexEmail.test(email) && password.length >= 6;
+    return isValid;
+  }
+
+  const handleToGoBack = () => {
+    setLoginIsNotVisible(false)
+  }
+
+  const handleChangePassword = () => {
+    setLoginIsNotVisible(true)
   }
 
   const handleLogin = async (e: React.FormEvent) => {
@@ -30,27 +45,68 @@ export default function Login() {
     }
   }
 
+  const handleNewPassword = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.put('http://localhost:3000/login', {
+          email,
+          password,
+      });
+      console.log(response.data);
+      setLoginIsNotVisible(false)
+      setEmail('')
+      setPassword('')
+  } catch (error) {
+      console.error('Error during Login:', error);
+  }    
+
+  }
+
   return (
     <div>
-        <h2>Login</h2>
-        <form onSubmit={handleLogin}>
-            <label>
-                UserName:
-                <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)}/>
-                <br />
-            </label>
-            <label>
-                Email:
-                <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
-                <br />
-            </label>
-            <label>
-                Password:
-                <input type="text" value={password} onChange={(e) => setPassword(e.target.value)}/>
-                <br />
-            </label>
-            <button type="submit" disabled={!formIsValid()} >Login</button>
-        </form>
+      {!loginIsNotVisible && (
+        <>
+          <h2>Login</h2>
+          <form onSubmit={handleLogin}>
+              <label>
+                  UserName:
+                  <input type="text" value={userName} onChange={(e) => setUserName(e.target.value)}/>
+                  <br />
+              </label>
+              <label>
+                  Email:
+                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                  <br />
+              </label>
+              <label>
+                  Password:
+                  <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                  <br />
+              </label>
+              <button type="submit" disabled={!formIsValid()} >Login</button>
+          </form>
+          <button type="button" onClick={handleChangePassword}>Forgot my password</button>
+        </>
+      )}
+      {loginIsNotVisible && (
+        <>
+          <form onSubmit={handleNewPassword}>
+              <label>
+                  Email:
+                  <input type="email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                  <br />
+              </label>
+              <label>
+                  New Password:
+                  <input type="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                  <br />
+              </label>
+              <button type="submit" disabled={!formNewPasswordIsValid()}>Change Password</button>
+              <button type="button" onClick={handleToGoBack}>To go back</button>
+          </form>
+        </>
+      )}
     </div>
   )
 }
